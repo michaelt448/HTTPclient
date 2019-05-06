f0aa6b8386192c72aa733ce33a86f992704b3dd2
Michael <michaeltilman77@gmail.com>
2019-05-06 12:13:37 -0700

    printing all the chuncks of data without any spaces, uses a variable to keep track

---
+++ printFilteredChuncks.js
@@ -0,0 +1,28 @@
+var https = require('https');
+
+function getAndPrintHTMLChunks () {
+  let bufferingVariable;
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step2.html'
+  };
+
+  /* Add your code here */
+
+console.log(requestOptions.host + requestOptions.path);
+  https.get('https://' + requestOptions.host + requestOptions.path,function(response) {
+    if(response.statusCode !== 200) {
+      console.log('You have a status problem' , response.status);
+    }
+    // response.setEncoding('utf8');
+    response.setEncoding('utf8');
+    response.on('data', function(data) {
+      bufferingVariable += data
+      console.log(bufferingVariable + '/n' + '/n' + '/n' + '/n' + '/n' + '/n' );
+    })
+  });
+
+}
+
+
+getAndPrintHTMLChunks();
